5b4266645f1253cf15fe3050a637b0fb7846d0c7
taiyebnirjhar <taiyebnirjhor@gmail.com>
2024-04-28 20:34:05 +0600

    update

--- vite-project/src/Layout/DashboardLayout/DashboardLayout.jsx
+++ vite-project/src/Layout/DashboardLayout/DashboardLayout.jsx
@@ -5,46 +5,48 @@ import { useState } from "react";
 // import SideBar from "./DashShared/SideBar/SideBar";
 // import ToTop from "../MainLayout/Shared/ToTop/ToTop";
 import { Helmet } from "react-helmet";
-import Sidebar from "../Sidebar/Sidebar";
 import { Outlet } from "react-router-dom";
 import DashNav from "../DashNav/DashNav";
-
+import Sidebar from "../Sidebar/Sidebar";
 
 const DashboardLayout = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const toggleSidebar = () => {
-      setIsSidebarOpen(!isSidebarOpen);
-      // console.log(isSidebarOpen);
-    };
-    return (
-      <>
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+    // console.log(isSidebarOpen);
+  };
+  return (
+    <>
       <Helmet>
-          <title>SHREYU | Dashboard</title>
-        
+        <title>SHREYU | Dashboard</title>
       </Helmet>
-     
-        <div className="flex">
-        
-          {/* Sidebar of dashboard starts here */}
-          <div
-            className={`side_bar bg-[#f9f9f9f5] min-h-screen fixed w-64 xl:min-w-[300px] z-[99999] text-white transition-all duration-300 ${isSidebarOpen ? "-ml-64 xl:ml-0" : "xl:-ml-[300px]"
-              }`}
-          >
-            <Sidebar></Sidebar>
-          </div>
-          {/* Main content of Dashboard starts here */}
-          <div className={`main_content w-svw transition-all duration-300 ${isSidebarOpen ? "xl:ml-[300px]" : ""}`}>
-            <DashNav toggleSidebar={toggleSidebar} ></DashNav>
-            <div className="flex-1">
-              <Outlet></Outlet>
-              <div className="fixed z-[99999] right-8 bottom-12">
-                {/* <ToTop></ToTop> */}
-              </div>
+
+      <div className="flex ">
+        {/* Sidebar of dashboard starts here */}
+        <div
+          className={`side_bar bg-[#f9f9f9f5] min-h-screen fixed w-64 xl:min-w-[300px] z-[99999] text-white transition-all duration-300  ${
+            isSidebarOpen ? "-ml-64 xl:ml-0" : "xl:-ml-[300px]"
+          }`}
+        >
+          <Sidebar />
+        </div>
+        {/* Main content of Dashboard starts here */}
+        <div
+          className={`main_content w-svw transition-all duration-300 ${
+            isSidebarOpen ? "xl:ml-[300px]" : ""
+          }`}
+        >
+          <DashNav toggleSidebar={toggleSidebar}></DashNav>
+          <div className="flex-1">
+            <Outlet></Outlet>
+            <div className="fixed z-[99999] right-8 bottom-12">
+              {/* <ToTop></ToTop> */}
             </div>
           </div>
         </div>
-      </>
-    );
-  };
+      </div>
+    </>
+  );
+};
 
 export default DashboardLayout;
--- vite-project/src/Layout/Sidebar/Sidebar.jsx
+++ vite-project/src/Layout/Sidebar/Sidebar.jsx
@@ -1,25 +1,79 @@
-import React from "react";
+import { useState } from "react";
 import { FaRegHospital } from "react-icons/fa";
-import Home from "../DashboardRoutes/Home/Home";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { Link } from "react-router-dom";
-
 const Sidebar = () => {
+  const [openDashboard, setOpenDashboard] = useState(true);
+
   return (
-    <div>
-      <div className="text-black flex justify-center items-center gap-2 my-6 text-xl font-semibold">
-        <FaRegHospital /> SHERE
+    <div className="border-red-600 p-4 pt-0 ">
+      <div className="text-black inline-flex gap-x-3 items-center my-6 text-xl font-semibold">
+        <span className="self-center">
+          <FaRegHospital />
+        </span>
+        <span>SHERE</span>
       </div>
-      <div className="text-black">
-        <Link to="/home">Home</Link>
-      </div>
-      <div className="text-black">
-        <Link to="/patients">Patients</Link>
-      </div>
-      <div className="text-black">
-        <Link to="/contracts">Contracts</Link>
-      </div>
-      <div className="text-black">
-        <Link to="/role">Roles & Users</Link>
+
+      <div className="">
+        <button
+          type="button"
+          className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group "
+          aria-controls="dropdown-example"
+          data-collapse-toggle="dropdown-example"
+          onClick={() => setOpenDashboard(!openDashboard)}
+        >
+          <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">
+            Dashboard
+          </span>
+          {!openDashboard && <IoIosArrowDown className="w-5 h-5" />}
+          {openDashboard && <IoIosArrowUp className="w-5 h-5" />}
+        </button>
+        <ul
+          id="dropdown-example"
+          className={`${
+            openDashboard
+              ? "block transition-all ease-in-out duration-300"
+              : "hidden transition-all ease-in-out duration-300"
+          } py-2 space-y-2 `}
+        >
+          <li>
+            <div className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group ">
+              <div className="text-black">
+                <Link to="/contracts">Contracts</Link>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group ">
+              <div className="text-black">
+                <Link to="/patients">Patients</Link>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group ">
+              <div className="text-black">
+                <Link to="/home">Home Visits</Link>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group ">
+              <div className="text-black">
+                <Link to="/contracts">Roles & Users</Link>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group ">
+              <div className="text-black">
+                <Link to="/role">Settings</Link>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
